7f1c8e8dfe9b9c5642173bddd2b41e0b3c01cfe5
idir-44 <idirmoudache+github@gmail.com>
2024-03-11 21:18:32 +0100

    feat: dockerise python app

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pyspark import SparkContext
 from operator import add
  
+if __name__ == "__main__":
+	sc = SparkContext(appName="HelloWorld")
+	data = sc.parallelize(list("Hello World"))
+	counts = data.map(lambda x: (x, 1)).reduceByKey(add).sortBy(lambda x: x[1], ascending=False).collect()
+	for (word, count) in counts:
+		print("{}: {}".format(word, count))
+	sc.stop()
 
-data = sc.parallelize(list("Hello World"))
-counts = data.map(lambda x: 
-	(x, 1)).reduceByKey(add).sortBy(lambda x: x[1],
-	 ascending=False).collect()
-
-for (word, count) in counts:
-    print("{}: {}".format(word, count))
