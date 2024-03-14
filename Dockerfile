f2e9e67a9fd1091e5b111bda0a47fee5d344fdc8
idir-44 <idirmoudache+github@gmail.com>
2024-03-15 00:34:59 +0100

    feat: add fastAPI

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 # syntax=docker/dockerfile:1
 
 
-
 FROM openjdk:17.0.1-jdk-slim as java-stage
 
 
@@ -9,10 +8,14 @@ RUN apt-get update && \
     apt-get install -y software-properties-common && \
     apt-get install -y python3-pip
 
-RUN python3 -m pip install pyspark
+RUN python3 -m pip install pyspark && \
+    python3 -m pip install fastapi && \
+    python3 -m pip install "uvicorn[standard]"
 
 WORKDIR /app
 
 COPY . .
 
-CMD python3 main.py
+EXPOSE 8080
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from pyspark import SparkContext
 from operator import add
  
-if __name__ == "__main__":
-	sc = SparkContext(appName="HelloWorld")
-	data = sc.parallelize(list("Hello World"))
-	counts = data.map(lambda x: (x, 1)).reduceByKey(add).sortBy(lambda x: x[1], ascending=False).collect()
-	for (word, count) in counts:
-		print("{}: {}".format(word, count))
-	sc.stop()
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+	sc = SparkContext(appName="HelloWorld")
+	rdd = sc.parallelize(["Hello", "World"])
+	result = rdd.reduce(add)
+	return result
